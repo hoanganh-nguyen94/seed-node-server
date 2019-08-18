098398cecf43f76732f9284f900c8bf8455dce0c
hoanganh-nguyen94 <hoanganh.nguyen@the-hash.com>
2019-08-18 19:48:53 +0700

    config graphql

--- src/server.js
+++ src/server.js
@@ -1,29 +1,25 @@
-import config from './config';
-import {ApolloServer} from 'apollo-server';
+import express from 'express';
 import mongoose from 'mongoose';
+import http from 'http';
+import config from './config';
+import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
 import TaskSchema from "./graphql/taskSchema";
 
-const server = new ApolloServer({
+const app = express();
+app.use('*', cors());
+app.use('/graphql', cors(), graphqlHTTP({
     schema: TaskSchema,
     rootValue: global,
     graphiql: true,
-});
+}));
 
+const server = http.createServer(app);
 
-mongoose.set('useFindAndModify', false);
+mongoose.connect(config.MONGO_URI, {promiseLibrary: require('bluebird'), useNewUrlParser: true})
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.error(err));
 
-mongoose
-    .connect(config.MONGO_URI, {useNewUrlParser: true, autoIndex: false})
-    .then(async () => {
-
-        console.log(`MongoDb is running`);
-
-        server.listen(config.API_PORT).then(({url}) => {
-            console.log(`🚀  Server ready at ${url}`);
-        });
-
-    });
-
-mongoose.connection.on('error', err => {
-    console.log(err);
+server.listen(config.API_PORT, () => {
+    console.log(`🚀 Go to http://localhost:${config.API_PORT}/graphql to run queries!`);
 });
